ca3e672f456ed73b2249228082d5638be1585bfc
saritasakkad <akkadsaritas@gmail.com>
2018-01-17 00:45:27 +0100

    Filtres

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
 angular .module('app', [])
         .controller('mainCtrl', ["$scope", function($scope){
-        
-            $scope.teams = [
-                {name: "OL", pts: 54, j: 26, g: 16, n: 6},
-                {name: "PSG", pts: 52, j: 26, g: 14, n: 10},
-                {name: "OM", pts: 58, j: 26, g: 15, n: 5},
-                {name: "Monaco", pts: 43, j: 25, g: 12, n: 7},
-                {name: "Saint-Etienne", pts: 42, j: 26, g: 11, n: 9},
-                {name: "Bordeaux", pts: 41, j: 26, g: 11, n: 7},
-                {name: "Montpellier", pts: 39, j: 25, g: 11, n: 8},
-                {name: "Guingamp", pts: 35, j: 26, g: 1, n: 13},
-            ];
+            $scope.value = 1337;
+            $scope.text = "Bonjour";
+            $scope.date = "1516230499000";
 
-            $scope.order = {
-                pts: "desc",
-                j: "desc",
-                g: "desc",
-                n: "desc"
+              $scope.users = [
+                  {name: "Sam"},
+                  {name: "Jim", email: "Sam@gmail.com"},
+                  {name: "Ben"}
+              ];
 
-            };
-
-            $scope.arrow = function(col) {
-                return $scope.order[col] == "desc" ? ">" : "<";
-            }
-
-            $scope.sort = function(col) {
-                $scope.teams.sort(function(a, b) {
-                    if ($scope.order[col] == "desc") {
-                        return a[col] - b[col];
-                    }
-                    else {
-                        return a[col] + b[col];
-                    }
-                });
-                $scope.order[col] = $scope.order[col] == "desc" ? "asc" : "desc"; 
-          }
+              $scope.filterValue = {name: "Sam"};
  }])
