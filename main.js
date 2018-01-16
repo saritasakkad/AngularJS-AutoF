fd3b23eff31daff66599c20c7bbaa7458dc686ac
saritasakkad <akkadsaritas@gmail.com>
2018-01-16 14:45:57 +0100

    Hiérarchie des contrôleurs

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 angular .module("app", [])
         .controller("mainCtrl", ["$scope", function($scope){
             $scope.name = "Akkad";
+        }])
+        .controller("childCtrl", ["$scope", function($scope){
+            $scope.name = "le";
+        }])
+        .controller("grantCtrl", ["$scope", function($scope){
+            $scope.name = "king";
         }]);
