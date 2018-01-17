b474f30b6b816e8d1d3a8c8aae81f341c949eead
saritasakkad <akkadsaritas@gmail.com>
2018-01-17 16:41:15 +0100

    Suite exercice - Création d'un tableau de classement avec les filtres

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 angular .module('app', [])
         .controller('mainCtrl', ["$scope", function($scope){
-            $scope.value = 1337;
-            $scope.text = "Bonjour";
-            $scope.date = "1516230499000";
+       
+          $scope.teams = [
+                {name: "OL", pts: 54, j: 26, g: 16, n: 6},
+                {name: "PSG", pts: 52, j: 26, g: 14, n: 10},
+                {name: "OM", pts: 58, j: 26, g: 15, n: 5},
+                {name: "Monaco", pts: 43, j: 25, g: 12, n: 7},
+                {name: "Saint-Etienne", pts: 42, j: 26, g: 11, n: 9},
+                {name: "Bordeaux", pts: 41, j: 26, g: 11, n: 7},
+                {name: "Montpellier", pts: 39, j: 25, g: 11, n: 8},
+                {name: "Guingamp", pts: 35, j: 26, g: 1, n: 13},
+            ];
+          
+            $scope.order = {
+                pts: true,
+                j: true,
+                g: true,
+                n:true
+            };
 
-              $scope.users = [
-                  {name: "Sam"},
-                  {name: "Jim", email: "Sam@gmail.com"},
-                  {name: "Ben"}
-              ];
+            $scope.arrow = function(col) {
+                return $scope.order[col] ? ">" : "<";            }
 
-              $scope.filterValue = {name: "Sam"};
+          $scope.onSort = function(col) {
+            $scope.sort = col;
+            $scope.order[col] = !$scope.order[col];
+          }
 
-              $scope.order = "name";
-              $scope.reverse = true;
-
-              $scope.inverser = function() {
-                  $scope.reverse = !$scope.reverse;
-              }
- }])
+}]) 
