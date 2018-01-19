e20c1ca792c9fde63d50135b65ae19cfe761d566
saritasakkad <akkadsaritas@gmail.com>
2018-01-19 09:53:19 +0100

    p1 Cacher, montrer des éléments avec ng-if, ng-show, ng-hide et ng-switch

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
 angular .module('app', [])
         .controller('mainCtrl', ["$scope", function($scope){
        
-        
-
-        $scope.hasChanged = function() {
-            console.log("Changé");
-        }
-          
-            $scope.users = [
-                {name: "Sam", age: 26},
-                {name: "Jam", age: 46},
-                {name: "Kam", age: 16},
-                {name: "Lam", age: 76},
-            ];
+        $scope.myvar = "foo";
 
            
 }]) 
