5960f2a0aca3f2a1eb47f69d9abd5757c1baa2aa
saritasakkad <akkadsaritas@gmail.com>
2018-01-22 19:08:12 +0100

    Création d'un composant avec des onglets

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-angular .module('app', [])
-        .controller('mainCtrl', ["$scope", function($scope){
+var app = angular.module('app', [])
+
+app.controller('mainCtrl', function($scope){
        
-        $scope.colors = [
-                "black",
-                "red",
-                "blue",
-                "green",
-                "purple",
-                "cyan ",
-                "gold ",
-                "yellow",
-                "lime"
+        $scope.tabs = [
+                {title: "Home", content: "Home"},
+                {title: "Profile", content: "Profile"},
+                {title: "Messages", content: "Messages"},
+                {title: "Settings", content: "Settings"}
+                
         ];
 
+        $scope.active = 0;
+
+        $scope.setActive = function(index) {
+                $scope.active = index;
+        }
+
+        $scope.isActive = function(index) {
+                return $scope.active == index;
+        }
+
+        $scope.removeTab = function(index) {
+                $scope.tabs.splice(index, 1);
+        }
+
+        $scope.addTab = function() {
+                $scope.tabs.push({
+                        title: $scope.name,
+                        content: $scope.content
+                });
+        }
            
-}]) 
+}); 
